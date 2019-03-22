aae75d7be1e54b1306d75d89536c1386405b7750
Julius Bahr <julius@juliuss-macbook-pro.local>
2019-03-22 13:52:10 +0100

    Initial Commit

---
+++ FlickerSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerSearch
+//
+//  Created by Julius Bahr on 22.03.19.
+//  Copyright © 2019 Julius Bahr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
